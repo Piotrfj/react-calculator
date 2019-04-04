8e4601f7180bd4de5ddfd2d4b46dca897b5f505c
Piotr Fijałkiewicz <piotr.fjj@gmail.com>
2019-04-05 00:37:09 +0200

    Updates

---
+++ precache-manifest.c092441298145ab5661096b572479da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68113105afe7db52e2a7",
+    "url": "/Portfolio-spa/static/js/runtime~main.0a5e9570.js"
+  },
+  {
+    "revision": "10a8987745c7840ee690",
+    "url": "/Portfolio-spa/static/js/main.f242dcfb.chunk.js"
+  },
+  {
+    "revision": "abaebe86eeb78d00808c",
+    "url": "/Portfolio-spa/static/js/2.895f411c.chunk.js"
+  },
+  {
+    "revision": "10a8987745c7840ee690",
+    "url": "/Portfolio-spa/static/css/main.cc05dfd2.chunk.css"
+  },
+  {
+    "revision": "3bf697de90043e70975f6199622022f4",
+    "url": "/Portfolio-spa/index.html"
+  }
+];
